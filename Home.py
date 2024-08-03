07c3f9e709d07dde299f697d4a0e977daa2e003a
Yoshixi <davidchenyuhe@proton.me>
2024-08-03 18:09:13 -0400

    changing st.session.token a bit

--- Home.py
+++ Home.py
@@ -1,10 +1,11 @@
 import streamlit as st
-from pages import Portion_Tracker, login, Recipe_Bank, signup
+#from pages import Portion_Tracker, login, Recipe_Bank, signup
 
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
+if st.session.token == None:
+    st.session.token = 0 # nothing. will not validate anything at all
 
 st.write("Welcome to my app")
-
--- pages/signup.py
+++ pages/signup.py
@@ -9,33 +9,37 @@ from database import get_supabase_api
 supabase_api = get_supabase_api()
 # from utils.database import SupabaseAPI 
 
-st.title("Create a profile")
-st.subheader("Make a family profile and start saving your money and the environment")
-form = st.form('my_form')
+print(st.session_state.token)
+signupmenu()
 
-def createProfile():
-    username = st.session_state.un
-    password = st.session_state.pw
-    size = st.session_state.family
-    validity = True
-    if len(username) == 0:
-        form.error("Enter a valid username")
-        validity = False
-    if len(password) == 0:
-        form.error("Enter a valid password")
-        validity = False
-    if len(size) == 0:
-        form.error('Enter a valid family size')
-        validity = False
+def signupmenu():
+    st.title("Create a profile")
+    st.subheader("Make a family profile and start saving your money and the environment")
+    form = st.form('my_form')
 
-    if validity:
-        salt = bcrypt.gensalt()
-        hashed_passwd = bcrypt.hashpw(bytes(password,'utf-8'), salt).decode("utf-8")
-        supabase_api.add_user(username,hashed_passwd,size)
-        form.success("Account Created")
+    def createProfile():
+        username = st.session_state.un
+        password = st.session_state.pw
+        size = st.session_state.family
+        validity = True
+        if len(username) == 0:
+            form.error("Enter a valid username")
+            validity = False
+        if len(password) == 0:
+            form.error("Enter a valid password")
+            validity = False
+        if len(size) == 0:
+            form.error('Enter a valid family size')
+            validity = False
 
-with form:
-    st.text_input("Username", key="un")
-    st.text_input("Password", key="pw", type = "password")
-    st.text_input("Number of family members", key="family")
-    st.form_submit_button("Create Account", on_click=createProfile)
+        if validity:
+            salt = bcrypt.gensalt()
+            hashed_passwd = bcrypt.hashpw(bytes(password,'utf-8'), salt).decode("utf-8")
+            supabase_api.add_user(username,hashed_passwd,size)
+            form.success("Account Created")
+
+    with form:
+        st.text_input("Username", key="un")
+        st.text_input("Password", key="pw", type = "password")
+        st.text_input("Number of family members", key="family")
+        st.form_submit_button("Create Account", on_click=createProfile)
