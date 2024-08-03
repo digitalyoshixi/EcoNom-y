f6061a018c528e9142a9cbe292efdca77fe3547f
Yoshixi <davidchenyuhe@proton.me>
2024-08-03 19:09:45 -0400

    tootoo

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-if st.session_state.token == None:
+if 'token' not in st.session_state:
     st.session_state.token = 0 # nothing. will not validate anything at all
 
 st.write("Welcome to my app")
