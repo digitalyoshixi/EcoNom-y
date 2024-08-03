e671e412c139caa335aea7d82d65ba126d289764
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-08-03 13:06:03 -0400

    More

--- utils/database.py
+++ utils/database.py
@@ -3,14 +3,10 @@ from supabase import create_client, Client
 
 
 class SupabaseAPI:
-    project_url: str
-    api_key: str
     database: Client
 
     def __init__(self, project_url: str, api_key: str):
-        self.project_url = project_url
-        self.api_key = api_key
-        self.database = create_client(url, key)
+        self.database = create_client(project_url, api_key)
 
 
 if __name__ == "__main__":
@@ -18,3 +14,5 @@ if __name__ == "__main__":
 
     project_url: str = os.environ["SUPABASE_PROJECT_URL"]
     api_key: str = os.environ["SUPABASE_API_KEY"]
+
+    supabase_api = SupabaseAPI(project_url, api_key)
