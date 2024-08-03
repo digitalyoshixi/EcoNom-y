b28e6ff6e2b2a129957cfcd7b0b7de88be4ccf1e
David Jiang <jiang.david23@gmail.com>
2024-08-03 10:55:45 -0400

    added title and substitle

--- portionTracker.py
+++ portionTracker.py
@@ -1,3 +1,8 @@
 import streamlit as st
-st.write("Hello World")
 
+st.title("Portion Tracker")
+st.subheader("Upload an image of your meal and EcoNom-y will help you to save money and food!")
+
+
+
+#image upload (picture of your meal)
