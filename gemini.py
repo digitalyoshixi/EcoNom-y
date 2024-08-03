f3b08d56ea0dccc5b2d3387a31edadd09dde6564
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-08-03 11:45:28 -0400

    Slight refactor

--- gemini.py
+++ gemini.py
@@ -1,17 +1,25 @@
 import google.generativeai as genai
 import os
-import PIL.Image
+from PIL import Image, ImageFile
 
 class GeminiAPI():
-    def __init__(self, key):
-        self.key = key
-        genai.configure(api_key=key)
+    def __init__(self, api_key: str) -> None:
+        # Configure Gemini with API key
+        genai.configure(api_key=api_key)
         self.model = genai.GenerativeModel('gemini-1.5-flash')
     
-    def resimg(self, prompt, img):
-        # assuming imgobj is created like img = PIL.Image.open('image.jpg')
+    def resimg(self, prompt: str, img: ImageFile) -> str:
+        """
+        Sends an image object and prompt to Google Gemini, returns the response.
+        
+        Parameters:
+            prompt: The prompt to send Gemini.
+            imgobj: An image object given by PIL.Image.open
+        """
+        # assuming imgobj is created like img =('image.jpg')
         response = self.model.generate_content([prompt, img])
-        print(response)
+        
+        return response.text
 
     def restxt(self, prompt):
         response = self.model.generate_content(prompt)
@@ -26,5 +34,6 @@ if __name__ == "__main__":
     
     gemini_api = GeminiAPI(os.environ["GOOGLE_GEMINI_API_KEY"])
 
-    testimg = PIL.Image.open('testgemini.png')
-    gemini_api.resimg(testimg,"what do u think about this?")
+    testimg = Image.open('testgemini.png')
+    response = gemini_api.resimg(testimg,"what do u think about this?")
+    print(response)
