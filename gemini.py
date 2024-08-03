fef49e38a29043d2bc5335025dba53c1874dc9e7
Yoshixi <davidchenyuhe@proton.me>
2024-08-03 11:21:12 -0400

    gemini oop

--- gemini.py
+++ gemini.py
@@ -1,17 +1,18 @@
 import google.generativeai as genai
-from dotenv import load_dotenv
 import os
-load_dotenv()
-key = os.environ["GOOGLE_API_KEY"]
-print(key)
+    
+
+class GeminiAPI():
+    def __init__(self, key):
+        self.key = key
+    
+    def recimg(self, imgobj):
+        GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
+        genai.configure(api_key=GOOGLE_API_KEY)
 
 
-# GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
-# genai.configure(api_key=GOOGLE_API_KEY)
-
-
-def imagenanalysis(inp):
-    print("balls")
-
-def textanalysis(inp):
-    print("balls")
+if __name__ == "__main__":
+    from dotenv import load_dotenv    
+    load_dotenv()
+    key = os.environ["GOOGLE_API_KEY"]
+    geminiapi = GeminiAPI(key)
