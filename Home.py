6120cca66a45d2c33d32edfc4e2393738e963216
Moiz Choudhary <moizc80@gmail.com>
2024-08-03 19:42:22 -0400

    improved home page

--- Home.py
+++ Home.py
@@ -1,11 +1,12 @@
 import streamlit as st
 #from pages import Portion_Tracker, login, Recipe_Bank, signup
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-if st.session_state.token == None:
-    st.session_state.token = 0 # nothing. will not validate anything at all
+st.markdown("<h1 style='text-align: center;'>EcoNom-y</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Save Food, Save Money</h3>", unsafe_allow_html=True)
 
-st.write("Welcome to my app")
+st.markdown("<h4 style='text-align: left;'>Our Goal:</h4>", unsafe_allow_html=True)
+st.markdown("Help consumers reduce their food waste")
+#if st.session_state.token == None:
+    #st.session_state.token = 0 # nothing. will not validate anything at all
+
+
