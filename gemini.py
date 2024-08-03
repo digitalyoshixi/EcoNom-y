4e04ad2185239c972621005ffa6ae5cd00488381
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-08-03 11:39:42 -0400

    Rename GOOGLE_API_KEY to GOOGLE_GEMINI_API_KEY

--- gemini.py
+++ gemini.py
@@ -20,8 +20,11 @@ class GeminiAPI():
 
 if __name__ == "__main__":
     from dotenv import load_dotenv    
+    
+    # Load environment variables from .env
     load_dotenv()
-    key = os.environ["GOOGLE_API_KEY"]
-    geminiapi = GeminiAPI(key)
+    
+    gemini_api = GeminiAPI(os.environ["GOOGLE_GEMINI_API_KEY"])
+
     testimg = PIL.Image.open('testgemini.png')
-    geminiapi.resimg(testimg,"what do u think about this?")
+    gemini_api.resimg(testimg,"what do u think about this?")
