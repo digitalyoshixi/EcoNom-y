b894bbc84c937331b9aa1b021387edec2f507555
Yoshixi <davidchenyuhe@proton.me>
2024-08-03 11:14:55 -0400

    secrets

--- gemini.py
+++ gemini.py
@@ -1,8 +1,14 @@
 import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+key = os.environ["GOOGLE_API_KEY"]
+print(key)
 
-from google.colab import userdata
-GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
-genai.configure(api_key=GOOGLE_API_KEY)
+
+# GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
+# genai.configure(api_key=GOOGLE_API_KEY)
+
 
 def imagenanalysis(inp):
     print("balls")
