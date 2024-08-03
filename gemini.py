9029e41f38e59991d1923d5132233404fbdaf7f7
Yoshixi <davidchenyuhe@proton.me>
2024-08-03 11:09:59 -0400

    cool

--- gemini.py
+++ gemini.py
@@ -1,4 +1,8 @@
-import requests
+import google.generativeai as genai
+
+from google.colab import userdata
+GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
 
 def imagenanalysis(inp):
     print("balls")
