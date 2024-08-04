b8c2f8cc192a2fb6f5e7f27da7497129b9fc5b07
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-08-03 20:03:41 -0400

    Create receipt parser

--- utils/receipt.py
+++ utils/receipt.py
@@ -2,21 +2,19 @@ import PIL
 import json_repair
 from gemini import GeminiAPI
 
-# IMAGE_PROMPT = """Parse this receipt for cooking ingredients and return
-# the contents in JSON format. Ignore ALL non-food items.
+IMAGE_PROMPT = """Parse this receipt for the items and return them in JSON format. Only look for food items that you know of, and return the ingredient in it's simplest format.
 
-# [
-#     {
-#         "ingredient": "Cabbage",
-#         "quantity": "1"
-#     },
-#     {
-#         "ingredient": "...",
-#         "quantity": "#"
-#     }
-# ]"""
-
-IMAGE_PROMPT = "What do you see?"
+JSON Format:
+[
+    {
+        "ingredient": "Cabbage",
+        "quantity": "1"
+    },
+    {
+        "ingredient": "...",
+        "quantity": "#"
+    }
+]"""
 
 
 class ReceiptParser:
@@ -26,11 +24,14 @@ class ReceiptParser:
         self.gemini_api = GeminiAPI(gemini_api_key)
 
     def parse_receipt(self, receipt: PIL.ImageFile) -> list[dict[str, str]]:
-        """
+        f"""
         Sends a receipt to Google Gemini, returns the parsed data.
 
         Parameters:
             recipt: An image object given by PIL.Image.open.
+
+        Gemini Prompt:
+            {IMAGE_PROMPT}
         """
         response = self.gemini_api.image_response(
             prompt=IMAGE_PROMPT, image=receipt)
@@ -51,6 +52,6 @@ if __name__ == "__main__":
     response = requests.get("https://i.redd.it/kp1udlgk5zv91.jpg")
 
     image = Image.open(BytesIO(response.content))
-
     parsed_receipt = receipt_parser.parse_receipt(image)
-    print(parsed_receipt)
+    for item in parsed_receipt:
+        print(item['quantity'], item['ingredient'])
