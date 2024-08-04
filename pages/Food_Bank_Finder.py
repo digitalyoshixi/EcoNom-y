724220c9e47a36c035a115ebd2bdc36597edcf78
Moiz Choudhary <moizc80@gmail.com>
2024-08-03 21:04:00 -0400

    changed food bank finder a bit

--- pages/Food_Bank_Finder.py
+++ pages/Food_Bank_Finder.py
@@ -2,23 +2,26 @@ import streamlit as st
 import pandas as pd
 from utils.food_banks import FoodBanks
 
-#flat, flng
 food_banks = FoodBanks()
-query = food_banks.locate_food_banks("Toronto Metropolitian University")
-col1 = []
-col2 = []
 
-for food_bank in query['closest_food_banks']:
-    col1.append(food_bank['flat'])
-    col2.append(food_bank['flng'])
+def receive_address(address):
+    query = food_banks.locate_food_banks(address)
 
-df = pd.DataFrame({
-    "col1": col1,
-    "col2": col2})
+    col1 = []
+    col2 = []
+
+    for food_bank in query['closest_food_banks']:
+        col1.append(food_bank['flat'])
+        col2.append(food_bank['flng'])
+
+    df = pd.DataFrame({
+        "col1": col1,
+        "col2": col2})
+    
+    st.map(df,
+    latitude='col1',
+    longitude='col2')
 
 st.title("Food Banks Nearby")
 st.subheader("Donate unwanted ingredients")
 
-st.map(df,
-    latitude='col1',
-    longitude='col2')
