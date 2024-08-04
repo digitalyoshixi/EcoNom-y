0d2be9ff2ebb27d9224bdaa4d1618713fc713baf
Moiz Choudhary <moizc80@gmail.com>
2024-08-03 20:52:03 -0400

    added map to food bank finder

--- pages/Food_Bank_Finder.py
+++ pages/Food_Bank_Finder.py
@@ -5,11 +5,16 @@ from utils.food_banks import FoodBanks
 #flat, flng
 food_banks = FoodBanks()
 query = food_banks.locate_food_banks("Toronto Metropolitian University")
+col1 = []
+col2 = []
 
+for food_bank in query['closest_food_banks']:
+    col1.append(food_bank['flat'])
+    col2.append(food_bank['flng'])
 
 df = pd.DataFrame({
-    "col1": [query['closest_food_banks'][0]['flat'], query['closest_food_banks'][1]['flat']],
-    "col2": [query['closest_food_banks'][0]['flng'], query['closest_food_banks'][1]['flng']]})
+    "col1": col1,
+    "col2": col2})
 
 st.title("Food Banks Nearby")
 st.subheader("Donate unwanted ingredients")
